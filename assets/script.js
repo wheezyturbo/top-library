15e662dd1a47374d90c92b492ae8026645122694
rubyCiide <joelpaul900@gmail.com>
2023-11-06 19:40:40 +0530

    [UPDATE] improve readability

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,3 @@
-const add = document.querySelector(".btn-add");
-const form = document.querySelector(".modal-form");
-const modal = document.querySelector(".modal");
-const formadd = document.querySelector(".modal-btn");
-const cards = document.querySelector(".cards");
-
 class Book {
   constructor(name = "Untitled", author = "Unknown", pages = 0, read = false) {
     this.name = name;
@@ -21,75 +15,86 @@ class Library {
 }
 
 
-
-add.addEventListener("click", () => {
-  modal.classList.toggle("hidden");
-  document.querySelector(".cards").classList.toggle("hidden");
-  document.querySelector(".container").classList.toggle("blur");
-  add.textContent = add.textContent === "+" ? "-" : "+";
-  clearForm();
-});
-
-formadd.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  const name = document.getElementById("name").value;
-  const author = document.getElementById("author").value;
-  const pages = document.getElementById("pages").value;
-  const read = document.getElementById("read").checked;
-
-  const nameInput = document.getElementById("name");
-  if (!name) {
-    nameInput.placeholder = "Name is required!";
-    return;
+const DisplayController = (()=>{
+  const add = document.querySelector(".btn-add");
+  const modal = document.querySelector(".modal");
+  const formadd = document.querySelector(".modal-btn");
+  const cards = document.querySelector(".cards");
+  
+  
+  
+  add.addEventListener("click", () => {
+    modal.classList.toggle("hidden");
+    document.querySelector(".cards").classList.toggle("hidden");
+    document.querySelector(".container").classList.toggle("blur");
+    add.textContent = add.textContent === "+" ? "-" : "+";
+    clearForm();
+  });
+  
+  formadd.addEventListener("click", (e) => {
+    e.preventDefault();
+  
+    const name = document.getElementById("name").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked;
+  
+    const nameInput = document.getElementById("name");
+    if (!name) {
+      nameInput.placeholder = "Name is required!";
+      return;
+    }
+    nameInput.placeholder = "";
+  
+    Library.add(new Book(name, author, pages, read));
+    updateCards();
+    clearForm();
+  });
+  
+  function updateCards() {
+    cards.innerHTML = "";
+    Library.books.forEach((book, index) => {
+      const cardhtml = `<div class="card">
+          <h1 class="card-header">${book.name}</h1>
+          <p>Author: ${book.author}</p>
+          <p>Pages: ${book.pages}</p>
+          <p>Read: ${book.read ? "Yes" : "No"}</p>
+          <div class="card-btn">
+          <button data-index="${index}" class="remove-button">Remove</button>
+          <button data-index="${index}" class="toggle-read-button">Toggle Read Status</button>\
+          </div>
+        </div>`;
+  
+      cards.innerHTML += cardhtml;
+    });
+    const removeBtns = document.querySelectorAll(".remove-button");
+    const readBtns = document.querySelectorAll(".toggle-read-button");
+    removeBtns.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        const index = e.target.getAttribute("data-index");
+        Library.books.splice(index, 1);
+        updateCards();
+      });
+    });
+    readBtns.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        const index = e.target.getAttribute("data-index");
+        console.log(Library.books[index].read);
+        Library.books[index].read = !Library.books[index].read;
+        updateCards();
+      });
+    });
   }
-  nameInput.placeholder = "";
-
-  Library.add(new Book(name, author, pages, read));
+  
+  function clearForm() {
+    document.getElementById("name").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("pages").value = "";
+    document.getElementById("read").checked = false;
+  }
+  
   updateCards();
-  clearForm();
-});
+})()
 
-function updateCards() {
-  cards.innerHTML = "";
-  Library.books.forEach((book, index) => {
-    const cardhtml = `<div class="card">
-        <h1 class="card-header">${book.name}</h1>
-        <p>Author: ${book.author}</p>
-        <p>Pages: ${book.pages}</p>
-        <p>Read: ${book.read ? "Yes" : "No"}</p>
-        <div class="card-btn">
-        <button data-index="${index}" class="remove-button">Remove</button>
-        <button data-index="${index}" class="toggle-read-button">Toggle Read Status</button>\
-        </div>
-      </div>`;
 
-    cards.innerHTML += cardhtml;
-  });
-  const removeBtns = document.querySelectorAll(".remove-button");
-  const readBtns = document.querySelectorAll(".toggle-read-button");
-  removeBtns.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      const index = e.target.getAttribute("data-index");
-      Library.books.splice(index, 1);
-      updateCards();
-    });
-  });
-  readBtns.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      const index = e.target.getAttribute("data-index");
-      console.log(Library.books[index].read);
-      Library.books[index].read = !Library.books[index].read;
-      updateCards();
-    });
-  });
-}
 
-function clearForm() {
-  document.getElementById("name").value = "";
-  document.getElementById("author").value = "";
-  document.getElementById("pages").value = "";
-  document.getElementById("read").checked = false;
-}
-
-updateCards();
